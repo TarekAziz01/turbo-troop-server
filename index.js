0b9f3c893dc9c66791db7a0eb90920e1efcd9e53
Tarek Aziz <mdaziz3289@gmail.com>
2023-05-19 04:00:08 +0600

    POST method added for add toy

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shgh1ow.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get('/', (req, res) => {
-    res.send('Turbo Troop server is running...')
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toyCollection = client.db("toyDB").collection("toy");
+
+    app.post("/toys", async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+        const result = await toyCollection.insertOne(newToy);
+        res.send(result)
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Turbo Troop server is running...");
+});
 
 app.listen(port, () => {
-    console.log(`Turbo Troop server in running on port: ${port}`)
-})
+  console.log(`Turbo Troop server in running on port: ${port}`);
+});
