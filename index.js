b291e7cc1ccd29c91e148d5cc7c4c9c905d8822b
Tarek Aziz <mdaziz3289@gmail.com>
2023-05-20 18:36:39 +0600

    sinsel data det api added

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(query);
+      res.send(result)
+    })
+
 // read data by user email
     app.get('/mytoys', async (req, res) => {
       // console.log(req.query.email);
